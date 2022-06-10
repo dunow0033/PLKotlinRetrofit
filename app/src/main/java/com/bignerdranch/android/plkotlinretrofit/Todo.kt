a500f7405a5a948ca4d0ef85053f64961033966e
Brian Donnoe <dunow33@hotmail.com>
2022-06-09 20:28:37 -0400

    it's working

---
+++ app/src/main/java/com/bignerdranch/android/plkotlinretrofit/Todo.kt
@@ -0,0 +1,8 @@
+package com.bignerdranch.android.plkotlinretrofit
+
+data class Todo (
+    val completed: Boolean,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
---
+++ app/src/main/java/com/bignerdranch/android/plkotlinretrofit/TodoApi.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.plkotlinretrofit
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TodoApi {
+
+    @GET("/todos")
+    suspend fun getTodos(): Response<List<Todo>>
+}
